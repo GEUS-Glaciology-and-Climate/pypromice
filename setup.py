8e0808acdc9e3249df0f2c592c902890c97d1f15
Kenneth D. Mankoff <mankoff@gmail.com>
2022-05-11 14:25:23 -0700

    Fix missing required python packages

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=['numpy', 'pandas', 'xarray', 'toml'],
+    install_requires=['numpy', 'pandas', 'scipy', 'bottleneck', 'xarray', 'netcdf4', 'toml'],
     scripts=['bin/promiceAWS'],
 )
