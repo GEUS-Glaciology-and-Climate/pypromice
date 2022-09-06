Precipitation invalid masking bug solved

src/pypromice/aws.py
@@ -89,7 +89,7 @@ class AWS(object):
         print('Level 1 processing...')
         self.L0 = [addBasicMeta(item, self.vars) for item in self.L0]
         self.L1 = [toL1(item) for item in self.L0]
-        self.L1A = mergeVars(self.L1, self.vars)
+        self.L1A = mergeVars(self.L1, self.vars)   
         
         # L1 to L2 processing
         print('Level 2 processing...')
@@ -343,10 +343,10 @@ def mergeVars(ds_list, variables, cols=['lo','hi','OOL']):                     #
     # Get variables
     df = variables[cols]
     df = df.dropna(how='all')
-    
+
     # Remove outliers
     ds = clipValues(ds, df, cols)
-                        
+                      
     # Clean up metadata
     for k in ['format', 'hygroclip_t_offset', 'dsr_eng_coef', 'usr_eng_coef',
               'dlr_eng_coef', 'ulr_eng_coef', 'pt_z_coef', 'pt_z_p_coef',
@@ -386,21 +386,28 @@ def clipValues(ds, df, cols=['lo','hi','OOL']):
     for var in df.index:
         if var not in list(ds.variables): 
             continue
+        
         if var in ['rh_u_cor', 'rh_l_cor']:
              ds[var] = ds[var].where(ds[var] >= df.loc[var, lo], other = 0)
              ds[var] = ds[var].where(ds[var] <= df.loc[var, hi], other = 100)
         else:
-            ds[var] = ds[var].where(ds[var] >= df.loc[var, lo])
-            ds[var] = ds[var].where(ds[var] <= df.loc[var, hi])
-        other_vars = df.loc[var][ool] # either NaN or "foo" or "foo bar baz ..."
-        if isinstance(other_vars, str): 
+            if ~np.isnan(df.loc[var, lo]):
+                ds[var] = ds[var].where(ds[var] >= df.loc[var, lo])
+            if ~np.isnan(df.loc[var, hi]):                
+                ds[var] = ds[var].where(ds[var] <= df.loc[var, hi])
+                
+        other_vars = df.loc[var][ool]
+        if isinstance(other_vars, str) and ~ds[var].isnull().all():            # TODO change this to accomodate for instances where all values are flagged and nan'd prior
             for o in other_vars.split():
                 if o not in list(ds.variables): 
                     continue
-                ds[o] = ds[o].where(ds[var] >= df.loc[var, lo])
-                ds[o] = ds[o].where(ds[var] <= df.loc[var, hi])  
+                else:
+                    if ~np.isnan(df.loc[var, lo]):
+                        ds[o] = ds[o].where(ds[var] >= df.loc[var, lo])
+                    if ~np.isnan(df.loc[var, hi]):  
+                        ds[o] = ds[o].where(ds[var] <= df.loc[var, hi])  
     return ds
-
+    
 def popCols(ds, names):       
     for v in names:
         if v not in list(ds.variables):
@@ -669,15 +676,15 @@ class TestProcess(unittest.TestCase):
 #------------------------------------------------------------------------------
 
 if __name__ == "__main__": 
-    config_file = '../test/test_config1.toml'
-    inpath= '../test/'
-    outpath = '../test/'  
-    pAWS_gc = AWS(config_file, inpath, outpath)
+    # config_file = '../test/test_config1.toml'
+    # inpath= '../test/'
+    # outpath = '../test/'  
+    # pAWS_gc = AWS(config_file, inpath, outpath)
     
-    config_file = '../test/test_config2.toml'
-    inpath= '../test/'
-    outpath = '../test/'  
-    pAWS_gc = AWS(config_file, inpath, outpath)
+    # config_file = '../test/test_config2.toml'
+    # inpath= '../test/'
+    # outpath = '../test/'  
+    # pAWS_gc = AWS(config_file, inpath, outpath)
 
 
     # c = glob.glob('test/config/*.toml')
@@ -693,3 +700,14 @@ if __name__ == "__main__":
     #     print(name)
     #     inpath= '/home/pho/python_workspace/promice/aws-data/level_0/' + name       
     #     pAWS_prom = AWS(config_file, inpath, outpath)
+
+    c = glob.glob('/home/pho/python_workspace/promice/aws-data/tx/config/JAR_O*.toml')
+    inpath= '/home/pho/python_workspace/promice/aws-data/tx/' 
+    # outpath = '/home/pho/python_workspace/promice/aws-l3/level_3'
+    for config_file in list(c):
+        name = config_file.split('.toml')[0].split('/')[-1]    
+        pAWS = AWS(config_file, inpath, outpath=None)
+        # print(pAWS.L0[0]['precip_u'])
+        # print(pAWS.L1[0]['precip_u'])
+        # print(pAWS.L1A['precip_u'])
+        # print(pAWS.L2['precip_u'])