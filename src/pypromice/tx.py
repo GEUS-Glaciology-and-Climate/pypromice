eec5bf6da230e6a6e3fad885b3fdb854d8656025
Patrick Wright <pajwr@geus.dk>
2023-01-04 18:13:51 +0100

    persist uid in ini file (#77)
    Modification to getL0tx will write the previous last_uid back to the ini file if no new messages are found. The modification to tx.py is simply to improve the print statement. The last_uid should not be included in the list of new UIDs that is printed. As commented in the code, the search command always returns at least the most recent message, even if it has already been processed. So just remove from the print (it is also skipped in the processing).
    
    Resolves: #76

--- src/pypromice/tx.py
+++ src/pypromice/tx.py
@@ -751,7 +751,10 @@ def getMail(mail_server, last_uid=1):
     command = '(UID {}:*)'.format(last_uid)
     result, data = mail_server.uid('search', None, command)
     messages = data[0].split()
-    print('new UIDs: %s' %data[0].decode())
+    new_uids = data[0].decode()
+    # drop the last_uid (it has already been processed)
+    new_uids = new_uids.replace(str(last_uid), '')
+    print('new UIDs: %s' % new_uids)
 
     # Yield mails
     for message_uid in messages:
