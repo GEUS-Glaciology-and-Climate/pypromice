78cbfc8796e66f01ba8c2e6903f8671a647bd27d
PennyHow <pho@geus.dk>
2023-03-07 09:29:43 +0100

    look-up table implemented in module

--- src/pypromice/get.py
+++ src/pypromice/get.py
@@ -8,27 +8,6 @@ import pandas as pd
 import xarray as xr
 import unittest, pkg_resources
 from datetime import datetime
-        
-
-def lookup_table(base_dois,
-                 server='https://dataverse.geus.dk'):
-    # Prime API
-    dataverse_server = server.strip("/")
-    api = NativeApi(dataverse_server)
-    
-    # Look through DOI entries
-    lookup_list = {}
-    for d in base_dois:
-        dataset = api.get_dataset(d)
-        
-        # Get file names and DOIs
-        f_list = dataset.json()['data']['latestVersion']['files']
-        for f in f_list:
-            fname = f['dataFile']['filename'].lower()
-            if '.csv' in fname or '.nc' in fname or '.txt' in fname:
-                link = _getURL(f['dataFile']['persistentId'])
-                lookup_list[fname] = link
-    return lookup_list       
    
 def aws_names():
     '''Return PROMICE and GC-Net AWS names that can be used in get.aws_data() 
@@ -47,10 +26,7 @@ def aws_data(aws_name):
     '''
     lookup = lookup_table(['doi:10.22008/FK2/IW73UU', 'doi:10.22008/FK2/GNYFUK'])
     assert aws_name.lower() in list(lookup.keys())
-    if '.csv' in aws_name.lower():
-        data = pd.read_csv(lookup[aws_name], index_col=0, parse_dates=True)
-    elif '.nc' in aws_name.lower():
-        data = xr.open_dataset(lookup[aws_name])
+    data = pd.read_csv(lookup[aws_name], index_col=0, parse_dates=True)
     return data        
   
 def watson_discharge(t='hour'):
@@ -59,7 +35,7 @@ def watson_discharge(t='hour'):
     Parameters
     ----------
     t : str
-        Temporal resolution of the data - "hour", "day" or "month"
+        Temporal resolution of the data - "hour", "day" or "year"
 
     Returns
     -------
@@ -67,28 +43,74 @@ def watson_discharge(t='hour'):
         Watson river discharge dataframe    
     '''
     lookup = lookup_table(['doi:10.22008/FK2/XEHYCM'])
-    if 'month' in t.lower():
-        link = lookup['watson river discharge (2006-2021) monthly.txt']
+    
+    if 'year' in t.lower():
+        link = lookup['watson river discharge (1949-2021) yearly.txt']
+        df = pd.read_csv(link, sep="\s+", skiprows=9, index_col=0)
+        
     elif 'daily' in t.lower() or 'day' in t.lower():
-        link = lookup['watson river discharge (2006-2021) daily.txt']     
+        link = lookup['watson river discharge (2006-2021) daily.txt']   
+        df = pd.read_csv(link, sep="\s+", parse_dates=[[0,1,2]])\
+                        .rename({"WaterFluxDiversOnly(m3/s)"        : "divers",
+                                "Uncertainty(m3/s)"                 : "divers_err",
+                                "WaterFluxDivers&Temperature(m3/s)" : "divers_t",
+                                "Uncertainty(m3/s).1"               : "divers_t_err",
+                                "WaterFluxCumulative(km3)"          : "Q",
+                                "Uncertainty(km3)"                  : "err"}, 
+                                axis='columns')
+        df['time'] = df.iloc[:,0]
+        df = df.set_index('time')
+        df.drop(columns=df.columns[0:1], axis=1, inplace=True) 
+        
     else:
         link = lookup['watson river discharge (2006-2021) hourly.txt']  
         
-    df = pd.read_csv(link, sep="\s+", parse_dates=[[0,1,2,3]])\
-                    .rename({"WaterFluxDiversOnly(m3/s)"         : "divers",
-                            "Uncertainty(m3/s)"                 : "divers_err",
-                            "WaterFluxDivers&Temperature(m3/s)" : "divers_t",
-                            "Uncertainty(m3/s).1"               : "divers_t_err",
-                            "WaterFluxCumulative(km3)"          : "Q",
-                            "Uncertainty(km3)"                  : "err"}, 
-                            axis='columns')
-    df = _getDFdatetime(df, list(df.iloc[:,0]))
+        df = pd.read_csv(link, sep="\s+", parse_dates=[[0,1,2,3]])\
+                        .rename({"WaterFluxDiversOnly(m3/s)"        : "divers",
+                                "Uncertainty(m3/s)"                 : "divers_err",
+                                "WaterFluxDivers&Temperature(m3/s)" : "divers_t",
+                                "Uncertainty(m3/s).1"               : "divers_t_err",
+                                "WaterFluxCumulative(km3)"          : "Q",
+                                "Uncertainty(km3)"                  : "err"}, 
+                                axis='columns')
+        df = _getDFdatetime(df, list(df.iloc[:,0]))            
+
     return df
+
+def lookup_table(base_dois,
+                 server='https://dataverse.geus.dk'):
+    '''Fetch dictionary of data files and download URLs from a DOI entry in the
+    GEUS Dataverse
+    
+    Parameters
+    ----------
+    base_dois : list
+        List of DOIs to search
+    server : str, optional
+        DOI server. The default is "https://dataverse.geus.dk"
+    '''
+    # Prime API
+    dataverse_server = server.strip("/")
+    api = NativeApi(dataverse_server)
+    
+    # Look through DOI entries
+    lookup_list = {}
+    for d in base_dois:
+        dataset = api.get_dataset(d)
+        
+        # Get file names and DOIs
+        f_list = dataset.json()['data']['latestVersion']['files']
+        for f in f_list:
+            fname = f['dataFile']['filename'].lower()
+            if '.csv' in fname or '.txt' in fname:
+                link = _getURL(f['dataFile']['persistentId'])
+                lookup_list[fname] = link
+    return lookup_list 
 
 def _getURL(persistentId, 
             base_link='https://dataverse.geus.dk/api/access/datafile/:persistentId?persistentId='):
     '''Return URL download link from persistentId attached to DOI'''
-    return base_link+persistentId
+    return base_link+persistentId  
 
 
 def _getDFdatetime(df, dt_str, dt_format='%Y %m %d %H'):
@@ -108,9 +130,9 @@ def _getDFdatetime(df, dt_str, dt_format='%Y %m %d %H'):
     df : pandas.DataFrame
         DataFrame with added datetime as index
     '''
-    dates = [datetime.strptime(str(d), '%Y %m %d %H') for d in dt_str]
-    df['Datetime'] = dates
-    df = df.set_index('Datetime')
+    dates = [datetime.strptime(str(d), dt_format) for d in dt_str]
+    df['time'] = dates
+    df = df.set_index('time')
     df.drop(columns=df.columns[0], axis=1, inplace=True)  
     return df
 
@@ -132,22 +154,16 @@ class TestGet(unittest.TestCase):
         '''Test AWS data retrieval'''
         kan_b = aws_data('kan_b_hour.csv')
         self.assertIsInstance(kan_b, pd.DataFrame)
-
-    # def testAWSnc(self):
-    #     '''Test AWS data retrieval'''
-    #     kan_b = aws_data('nuk_k_hour.nc')
-    #     self.assertIsInstance(kan_b, xr.DataArray)
         
     def testWatsonHour(self):
         '''Test Wason River discharge hourly data retrieval'''
         wh = watson_discharge()
         self.assertTrue(wh['Q']['2021-10-27 23:00:00']==5.48)
         
-    # def testWatsonDaily(self):
-    #     '''Test Wason River discharge daily data retrieval'''
-    #     wd = watson_discharge(t='day')
-    #     print(wd)
-        # self.assertTrue(wd['Q']['2021-10-27']==5.48)
+    def testWatsonDaily(self):
+        '''Test Wason River discharge daily data retrieval'''
+        wd = watson_discharge(t='day')
+        self.assertTrue(wd['Q']['2009-09-04 00:00:00']==4.72)
             
 if __name__ == "__main__": 
     unittest.main()
