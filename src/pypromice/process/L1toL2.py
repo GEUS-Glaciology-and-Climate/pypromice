1c5fd1ad38cf8e5d2cd12f5cb3bdadf53080744e
Rasmus Bahbah Nielsen <114926145+RasmusBahbah@users.noreply.github.com>
2023-08-08 11:29:10 +0200

    Printing Current Path

--- src/pypromice/process/L1toL2.py
+++ src/pypromice/process/L1toL2.py
@@ -238,8 +238,9 @@ def percentileQC(ds):
     
     file_path = '../qc/percentiles.db'
     script_path = os.path.abspath('../qc/compute_percentiles.py')
-    script_check = os.path.isfile(script_path)
-    print(f'Does compute_percentiles exist: {script_check}')
+    
+    current_path = os.getcwd()
+    print(f'This is my current Path {current_path}')    
     
     if not os.path.isfile(file_path):
         subprocess.run([script_path])
