8fca6c3eb596775a9a6b676599c2068add84bdde
Penny How <pho@geus.dk>
2023-11-30 13:12:59 -0200

    Enable downloadable documentation with readthedocs (#213)
    * Enable downloadable documentation with readthedocs
    
    * configuration version updated

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, GEUS Glaciology and Climate'
 author = 'GEUS Glaciology and Climate'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.3.2'
 
 
 # -- General configuration ---------------------------------------------------
