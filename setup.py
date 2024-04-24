711cf9f1238b5157c29f6f74595fe4d4185f9371
Baptiste Vandecrux <35140661+BaptisteVandecrux@users.noreply.github.com>
2024-04-24 20:02:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pypromice",
-    version="1.3.4",
+    version="1.3.5",
     author="GEUS Glaciology and Climate",
     description="PROMICE/GC-Net data processing toolbox",
     long_description=long_description,
