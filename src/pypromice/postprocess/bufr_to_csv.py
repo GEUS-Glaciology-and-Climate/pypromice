3601c39cc3ba2c41fb4f223806d952a59ca02bcf
Mads Christian Lund <maclu@geus.dk>
2024-08-23 13:34:25 +0200

    Updated bufr_to_csv.py to allow multiple input files

--- src/pypromice/postprocess/bufr_to_csv.py
+++ src/pypromice/postprocess/bufr_to_csv.py
@@ -1,15 +1,22 @@
 import argparse
 from pathlib import Path
 
+import pandas as pd
+
 from pypromice.postprocess.bufr_utilities import read_bufr_file
 
 
 def main():
     parser = argparse.ArgumentParser("BUFR to CSV converter")
-    parser.add_argument("path", type=Path)
+    parser.add_argument("path", type=Path, nargs='+')
     args = parser.parse_args()
 
-    print(read_bufr_file(args.path).to_csv())
+    paths = []
+    for path in args.path:
+        paths += list(path.parent.glob(path.name))
+
+    df = pd.concat([read_bufr_file(path) for path in paths])
+    print(df.to_csv())
 
 
 if __name__ == "__main__":
