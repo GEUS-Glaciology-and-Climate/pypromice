GEUS-Glaciology-and-Climate/pypromice

commit d1a6ac870976f58a593c0193c99a8b1622713f39

Moved and Renamed clipValue -> clip_values to separate module
* Use separate module to avoid circular imports when used in other modules.
* Renamed according to PEP8 https://peps.python.org/pep-0008/#function-and-variable-names

src/pypromice/process/value_clipping.py
@@ -1,0 +1,64 @@
+from typing import Sequence
+
+import numpy as np
+import pandas
+import xarray
+
+
+def clip_values(
+    ds: xarray.Dataset,
+    df: pandas.DataFrame,
+    cols: Sequence[str] = ("lo", "hi", "OOL"),
+):
+    """
+    Clip values in dataset to defined "hi" and "lo" variables from dataframe.
+    There is a special treatment here for rh_u and rh_l variables, where values
+    are clipped and not assigned to NaN. This is for replication purposes
+
+    Parameters
+    ----------
+    ds : `xarray.Dataset`
+        Dataset to clip hi-lo range to
+    df : `pandas.DataFrame`
+        Dataframe to retrieve attribute hi-lo values from
+
+    Returns
+    -------
+    ds : `xarray.Dataset`
+        Dataset with clipped data
+    """
+
+    df = df[cols]
+    df = df.dropna(how="all")
+    lo = cols[0]
+    hi = cols[1]
+    ool = cols[2]
+    for var in df.index:
+        if var not in list(ds.variables):
+            continue
+
+        if var in ["rh_u_cor", "rh_l_cor"]:
+            ds[var] = ds[var].where(ds[var] >= df.loc[var, lo], other=0)
+            ds[var] = ds[var].where(ds[var] <= df.loc[var, hi], other=100)
+
+            # Mask out invalid corrections based on uncorrected var
+            var_uncor = var.split("_cor")[0]
+            ds[var] = ds[var].where(~np.isnan(ds[var_uncor]), other=np.nan)
+
+        else:
+            if ~np.isnan(df.loc[var, lo]):
+                ds[var] = ds[var].where(ds[var] >= df.loc[var, lo])
+            if ~np.isnan(df.loc[var, hi]):
+                ds[var] = ds[var].where(ds[var] <= df.loc[var, hi])
+
+        other_vars = df.loc[var][ool]
+        if isinstance(other_vars, str) and ~ds[var].isnull().all():
+            for o in other_vars.split():
+                if o not in list(ds.variables):
+                    continue
+                else:
+                    if ~np.isnan(df.loc[var, lo]):
+                        ds[o] = ds[o].where(ds[var] >= df.loc[var, lo])
+                    if ~np.isnan(df.loc[var, hi]):
+                        ds[o] = ds[o].where(ds[var] <= df.loc[var, hi])
+    return ds

src/pypromice/process/aws.py
@@ -9,6 +9,9 @@ from typing import Sequence
 
 import numpy as np
 import warnings
+
+from pypromice.process.value_clipping import clip_values
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 import xarray as xr
@@ -95,7 +98,7 @@ class AWS(object):
         '''Perform L1 to L2 data processing'''
         logger.info('Level 2 processing...')
         self.L2 = toL2(self.L1A)
-        self.L2 = clipValues(self.L2, self.vars)
+        self.L2 = clip_values(self.L2, self.vars)
 
     def getL3(self):
         '''Perform L2 to L3 data processing, including resampling and metadata
@@ -505,7 +508,7 @@ def mergeVars(ds_list, variables, cols=['lo','hi','OOL']):                     #
     df = df.dropna(how='all')
 
     # Remove outliers
-    ds = clipValues(ds, df, cols)
+    ds = clip_values(ds, df, cols)
 
     # Clean up metadata
     for k in ['format', 'hygroclip_t_offset', 'dsr_eng_coef', 'usr_eng_coef',
@@ -516,57 +519,6 @@ def mergeVars(ds_list, variables, cols=['lo','hi','OOL']):                     #
             ds.attrs.pop(k)
     return ds
 
-def clipValues(ds, df, cols=['lo','hi','OOL']):
-    '''Clip values in dataset to defined "hi" and "lo" variables from dataframe.
-    There is a special treatment here for rh_u and rh_l variables, where values
-    are clipped and not assigned to NaN. This is for replication purposes
-
-    Parameters
-    ----------
-    ds : xarray.Dataset
-        Dataset to clip hi-lo range to
-    df : pandas.DataFrame
-        Dataframe to retrieve attribute hi-lo values from
-
-    Returns
-    -------
-    ds : xarray.Dataset
-        Dataset with clipped data
-    '''
-    df = df[cols]
-    df = df.dropna(how='all')
-    lo = cols[0]
-    hi = cols[1]
-    ool = cols[2]
-    for var in df.index:
-        if var not in list(ds.variables):
-            continue
-
-        if var in ['rh_u_cor', 'rh_l_cor']:
-             ds[var] = ds[var].where(ds[var] >= df.loc[var, lo], other = 0)
-             ds[var] = ds[var].where(ds[var] <= df.loc[var, hi], other = 100)
-
-             # Mask out invalid corrections based on uncorrected var
-             var_uncor=var.split('_cor')[0]
-             ds[var] = ds[var].where(~np.isnan(ds[var_uncor]), other=np.nan)
-
-        else:
-            if ~np.isnan(df.loc[var, lo]):
-                ds[var] = ds[var].where(ds[var] >= df.loc[var, lo])
-            if ~np.isnan(df.loc[var, hi]):
-                ds[var] = ds[var].where(ds[var] <= df.loc[var, hi])
-
-        other_vars = df.loc[var][ool]
-        if isinstance(other_vars, str) and ~ds[var].isnull().all():
-            for o in other_vars.split():
-                if o not in list(ds.variables):
-                    continue
-                else:
-                    if ~np.isnan(df.loc[var, lo]):
-                        ds[o] = ds[o].where(ds[var] >= df.loc[var, lo])
-                    if ~np.isnan(df.loc[var, hi]):
-                        ds[o] = ds[o].where(ds[var] <= df.loc[var, hi])
-    return ds
 
 def popCols(ds, names):
     '''Populate dataset with all given variable names

commit eddc1bd42076c35c1c169aa2a69f5820de8dafe2

Updated variable dependency handling (OOL)
OOL contains dependent variables (children)
* Added unit test functions for clip_values
* Add a helper class, DependencyGraph, for modelling the dependency relations to find the children closure and handle circular dependencies.
* Updated clip_values to set children variables to nan whenever a parent is nan.
* Fixed a bug in clipValues introduced in commit d1a6ac870976f58a593c0193c99a8b1622713f39, where child variables were not set to nan due to an erroneous change in variable names.

Fixed variables.csv
* Removed incorrect OOL relations from variables.csv. wspd_x_u, wspd_y_u... had wdir and wspd as children. The relation is the opposite.
* Removed double quotes around ool
* Removed comma between ool entries in wspeed

src/pypromice/process/value_clipping.py
@@ -1,6 +1,11 @@
+from typing import Dict, Set, Mapping
+
 import numpy as np
 import pandas
+import pandas as pd
 import xarray
+
+from pypromice.utilities.dependency_graph import DependencyGraph
 
 
 def clip_values(
@@ -26,27 +31,43 @@ def clip_values(
     """
     cols = ["lo", "hi", "OOL"]
     assert set(cols) <= set(var_configurations.columns)
+    # TODO: Check if this is necessary
+    # variable_limits = var_configurations[cols].dropna(how="all")
 
-    variable_limits = var_configurations[cols].dropna(how="all")
+    variable_limits = var_configurations[cols].assign(
+        dependents=lambda df: df.OOL.fillna("").str.split(),
+        # Find the closure of dependents using the DependencyGraph class
+        dependents_closure=lambda df: DependencyGraph.from_child_mapping(
+            df.dependents
+        ).child_closure_mapping(),
+    )
+
     for var, row in variable_limits.iterrows():
-
         if var not in list(ds.variables):
             continue
+        # TODO: Check if this is necessary
+        # I guess the nan flagging is already handled below
+        # What if rh_u_cor is nan?
+        # What if row.lo/hi is nan?
 
-        if ~np.isnan(row.lo):
-            ds[var] = ds[var].where(ds[var] >= row.lo)
-        if ~np.isnan(row.hi):
-            ds[var] = ds[var].where(ds[var] <= row.hi)
+        if var in ["rh_u_cor", "rh_l_cor"]:
+            ds[var] = ds[var].where(ds[var] >= row.lo, other=0)
+            ds[var] = ds[var].where(ds[var] <= row.hi, other=100)
 
-        other_vars = row.OOL
-        if isinstance(other_vars, str) and ~ds[var].isnull().all():
-            for o in other_vars.split():
-                if o not in list(ds.variables):
-                    continue
-                else:
-                    if ~np.isnan(row.lo):
-                        ds[var] = ds[var].where(ds[var] >= row.lo)
-                    if ~np.isnan(row.hi):
-                        ds[var] = ds[var].where(ds[var] <= row.hi)
+            # Mask out invalid corrections based on uncorrected var
+            var_uncor = var.rstrip("_cor")
+            ds[var] = ds[var].where(~np.isnan(ds[var_uncor]), other=np.nan)
+
+        else:
+            if ~np.isnan(row.lo):
+                ds[var] = ds[var].where(ds[var] >= row.lo)
+            if ~np.isnan(row.hi):
+                ds[var] = ds[var].where(ds[var] <= row.hi)
+
+        # Flag dependents as NaN if parent is NaN
+        for o in row.dependents_closure:
+            if o not in list(ds.variables):
+                continue
+            ds[o] = ds[o].where(ds[var].notnull())
 
     return ds

src/pypromice/utilities/dependency_graph.py
@@ -1,0 +1,101 @@
+from typing import Mapping, Set, MutableMapping, Optional
+
+import attr
+
+__all__ = [
+    "DependencyNode",
+    "DependencyGraph",
+]
+
+
+@attr.define
+class DependencyNode:
+    name: str = attr.field()
+    parents: Set["DependencyNode"] = attr.field(factory=set)
+    children: Set["DependencyNode"] = attr.field(factory=set)
+
+    def add_child(self, child: "DependencyNode"):
+        self.children.add(child)
+        child.parents.add(self)
+
+    def get_children_closure(self, closure: Optional[Set[str]] = None) -> Set[str]:
+        is_root = closure is None
+        if closure is None:
+            closure = set()
+        if self.name in closure:
+            return closure
+        closure.add(self.name)
+        for child in self.children:
+            closure |= child.get_children_closure(closure)
+
+        if is_root:
+            closure.remove(self.name)
+        return closure
+
+    def get_parents_closure(self, closure: Optional[Set[str]] = None) -> Set[str]:
+        is_root = closure is None
+        if closure is None:
+            closure = set()
+        if self.name in closure:
+            return closure
+        closure.add(self.name)
+        for parent in self.parents:
+            closure |= parent.get_parents_closure(closure)
+
+        if is_root:
+            closure.remove(self.name)
+        return closure
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+@attr.define
+class DependencyGraph:
+    nodes: MutableMapping[str, DependencyNode] = attr.field(factory=dict)
+
+    def add_node(self, name: str) -> DependencyNode:
+        if name not in self.nodes:
+            self.nodes[name] = DependencyNode(name)
+        return self.nodes[name]
+
+    def add_edge(self, parent: str, child: str):
+        parent_node = self.add_node(parent)
+        child_node = self.add_node(child)
+        parent_node.add_child(child_node)
+
+    @classmethod
+    def from_child_mapping(cls, mapping: Mapping[str, Set[str]]) -> "DependencyGraph":
+        graph = cls()
+        for var, children in mapping.items():
+            graph.add_node(var)
+            for child in children:
+                graph.add_edge(var, child)
+        return graph
+
+    @classmethod
+    def from_parent_mapping(cls, mapping: Mapping[str, Set[str]]) -> "DependencyGraph":
+        graph = cls()
+        for var, parents in mapping.items():
+            graph.add_node(var)
+            for parent in parents:
+                graph.add_edge(parent, var)
+        return graph
+
+    def child_mapping(self) -> Mapping[str, Set[str]]:
+        return {
+            node.name: {child.name for child in node.children}
+            for node in self.nodes.values()
+        }
+
+    def child_closure_mapping(self) -> Mapping[str, Set[str]]:
+        return {node.name: node.get_children_closure() for node in self.nodes.values()}
+
+    def parent_mapping(self) -> Mapping[str, Set[str]]:
+        return {
+            node.name: {parent.name for parent in node.parents}
+            for node in self.nodes.values()
+        }
+
+    def parent_closure_mapping(self) -> Mapping[str, Set[str]]:
+        return {node.name: node.get_parents_closure() for node in self.nodes.values()}

tests/unit/test_value_clippping.py
@@ -1,0 +1,200 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+import pypromice.resources
+from pypromice.process.L1toL2 import get_directional_wind_speed
+from pypromice.process.value_clipping import clip_values
+
+
+class ClipValuesTestCase(unittest.TestCase):
+    def test_flag_wdir_on_nan_wspd(self):
+        n_entries = 4
+        df_in = pd.DataFrame(
+            data={
+                "wspd_u": n_entries * [np.nan],
+                "wspd_i": n_entries * [np.nan],
+                "wspd_l": n_entries * [np.nan],
+                "wdir_u": np.random.rand(n_entries) * 360,
+                "wdir_i": np.random.rand(n_entries) * 360,
+                "wdir_l": np.random.rand(n_entries) * 360,
+            },
+            index=pd.date_range("2021-01-01", periods=n_entries, freq="H"),
+        )
+        ds = xr.Dataset(df_in)
+        ds.attrs["number_of_booms"] = 2
+        ds_out = get_directional_wind_speed(ds.copy())
+        vars = pypromice.resources.load_variables(None)
+
+        ds_out = clip_values(ds_out, vars)
+        # Convert to dataframe for easier comparison
+        df_out = ds_out.to_dataframe()
+
+        # Assert all dir values are set nan
+        self.assertTrue(df_out["wdir_u"].isna().all())
+        self.assertTrue(df_out["wdir_l"].isna().all())
+        self.assertTrue(df_out["wdir_i"].isna().all())
+
+    def test_flag_wdir_zero_wspd(self):
+        n_entries = 4
+        df_in = pd.DataFrame(
+            data={
+                "wspd_u": n_entries * [0],
+                "wspd_i": n_entries * [0],
+                "wspd_l": n_entries * [0],
+                "wdir_u": np.random.rand(n_entries) * 360,
+                "wdir_i": np.random.rand(n_entries) * 360,
+                "wdir_l": np.random.rand(n_entries) * 360,
+            },
+            index=pd.date_range("2021-01-01", periods=n_entries, freq="H"),
+        )
+        ds = xr.Dataset(df_in)
+        ds.attrs["number_of_booms"] = 2
+        ds_out = get_directional_wind_speed(ds.copy())
+        vars = pypromice.resources.load_variables(None)
+
+        ds_out = clip_values(ds_out, vars)
+
+        # Convert to dataframe for easier comparison
+        df_out = ds_out.to_dataframe()
+        # Assert all dir values are set nan
+        self.assertTrue(df_out["wdir_u"].isna().all())
+        self.assertTrue(df_out["wdir_l"].isna().all())
+        self.assertTrue(df_out["wdir_i"].isna().all())
+
+    def test_flagging_depended_on_wspd(self):
+        # This unit test tests variable conditions for flagging wdir values
+        # Nan, 0, and negative values should be flagged while positive values should not
+        n_entries = 4
+        df_in = pd.DataFrame(
+            data={
+                "wspd_u": [np.nan, 0, 10, -3],
+                "wdir_u": [0, 180, 90, 270],
+            },
+            index=pd.date_range("2021-01-01", periods=n_entries, freq="H"),
+        )
+        ds = xr.Dataset(df_in)
+        ds.attrs["number_of_booms"] = 1
+        ds_out = get_directional_wind_speed(ds.copy())
+        vars = pypromice.resources.load_variables(None)
+
+        ds_out = clip_values(ds_out, vars)
+
+        # Convert to dataframe for easier comparison
+        df_out = ds_out.to_dataframe()
+
+        expected_dataframe = pd.DataFrame(
+            data={
+                "wspd_u": [np.nan, 0, 10, np.nan],
+                "wdir_u": [np.nan, np.nan, 90, np.nan],
+                "wspd_x_u": [np.nan, np.nan, 10, np.nan],
+                "wspd_y_u": [np.nan, np.nan, 0, np.nan],
+            },
+            index=df_out.index,
+        )
+        self.assertEqual(
+            df_out.columns.tolist(),
+            expected_dataframe.columns.tolist(),
+        )
+        pd.testing.assert_frame_equal(
+            df_out,
+            expected_dataframe,
+            check_dtype=True,
+            check_like=True,
+        )
+
+    def test_recursive_flagging(self):
+        fields = ["a", "b", "c"]
+        variable_config = pd.DataFrame(
+            columns=["field", "lo", "hi", "OOL"],
+            data=[
+                ["a", 0, 10, "b"],
+                ["b", 100, 110, ""],
+                ["c", 200, 210, "a"],
+            ],
+        ).set_index("field")
+        data_index = pd.RangeIndex(4)
+        data = pd.DataFrame(
+            columns=fields,
+            data=[
+                [0, 100, 215],  # c is out of range
+                [5, 115, 200],  # b is out of range
+                [10, 100, 200],  # All a withing range
+                [15, 100, 200],  # a is out of range
+            ],
+            dtype=float,
+            index=data_index,
+        )
+        expected_output = pd.DataFrame(
+            columns=fields,
+            data=[
+                [np.nan, np.nan, np.nan],  # c is nan -> a -> b
+                [5, np.nan, 200],  # b is nan
+                [10, 100, 200],
+                [np.nan, np.nan, 200],  # a is nan -> b
+            ],
+            dtype=float,
+            index=data.index,
+        )
+
+        data_set = xr.Dataset(data)
+
+        data_set_out = clip_values(data_set, variable_config)
+        data_frame_out = data_set_out.to_dataframe()
+
+        pd.testing.assert_frame_equal(
+            data_frame_out,
+            expected_output,
+            check_names=False,
+            check_dtype=True,
+        )
+
+    def test_circular_dependencies(self):
+        fields = ["a", "b", "c"]
+        variable_config = pd.DataFrame(
+            columns=["field", "lo", "hi", "OOL"],
+            data=[
+                ["a", 0, 10, "b"],
+                ["b", 100, 110, "c"],
+                ["c", 200, 210, "a"],
+            ],
+        ).set_index("field")
+        data_index = pd.RangeIndex(4)
+        data = pd.DataFrame(
+            columns=fields,
+            data=[
+                [0, 100, 215],  # c is out of range
+                [5, 115, 200],  # b is out of range
+                [10, 100, 200],  # All a withing range
+                [15, 100, 200],  # a is out of range
+            ],
+            dtype=float,
+            index=data_index,
+        )
+        # All variables a dependent due to circular dependency
+        expected_output = pd.DataFrame(
+            columns=fields,
+            data=[
+                [np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan],
+                [10, 100, 200],
+                [np.nan, np.nan, np.nan],
+            ],
+            dtype=float,
+            index=data.index,
+        )
+
+        data_set = xr.Dataset(data)
+
+        data_set_out = clip_values(data_set, variable_config)
+        data_frame_out = data_set_out.to_dataframe()
+
+        pd.testing.assert_frame_equal(
+            data_frame_out,
+            expected_output,
+            check_names=False,
+            check_dtype=True,
+        )
+