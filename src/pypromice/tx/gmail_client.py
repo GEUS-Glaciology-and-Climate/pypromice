4a3d20a6ed6a9464bad47e3715fc3cf06d3cbb86
Mads Christian Lund <maclu@geus.dk>
2025-05-26 10:39:07 +0200

    Wrapping up gmail client

--- src/pypromice/tx/gmail_client.py
+++ src/pypromice/tx/gmail_client.py
@@ -32,8 +32,7 @@ class GmailClient:
         logger.info("Logged in to email server")
 
         result, data = self.mail_server.select(mailbox='"[Gmail]/All Mail"', readonly=True)
-        logger.info("Mailbox contains %s messages", data[0].decode())
-
+        logger.info('Mailbox "[Gmail]/All Mail" contains %s messages', data[0].decode())
 
     def __enter__(self):
         return self
@@ -43,18 +42,18 @@ class GmailClient:
 
     # E-Mail download methods
     # --------------------------------------------------------------------------------------------
-    def fetch_mail(self, uid: int) -> Message | None:
+    def fetch_mail(self, uid: str) -> Message | None:
         """Fetch a single email by Gmail UID."""
         return self.fetch_mails([uid]).__next__()
 
-    def fetch_mails(self, uids: list[int], chunk_size: int = 100) -> Iterator[Message]:
+    def fetch_mails(self, uids: list[str], chunk_size: int = 100) -> Iterator[Message]:
         """
         Fetch all messages corresponding to the given list of UIDs.
         The messages are returned in the same order as the UIDs.
         """
         yield from self._fetch_mail_chunks(uids)
 
-    def _fetch_mail_chunks(self, uids: List[int]) -> Iterator[Message]:
+    def _fetch_mail_chunks(self, uids: List[str]) -> Iterator[Message]:
         """Fetch mails in chunks and yield them as UID -> raw mail data mapping."""
         for i in range(0, len(uids), self.chunk_size):
             chunk = uids[i:i + self.chunk_size]
@@ -72,14 +71,14 @@ class GmailClient:
 
     # UID query methods
     # --------------------------------------------------------------------------------------------
-    def get_new_uids(self, last_uid:int) -> List[int]:
+    def get_new_uids(self, last_uid:str|int) -> List[str]:
         """
         Get new Gmail UIDs that are greater than the last known UID.
         """
         result, data = self.mail_server.uid("search", None, f"(UID {last_uid+1}:*)")
-        return list(map(int, data[0].split()))
+        return data[0].split()
 
-    def get_uids_by_date(self, date: datetime | str) -> list[int]:
+    def get_uids_by_date(self, date: datetime | str) -> list[str]:
         """
         Get Gmail UIDs for messages received on a specific date.
         """
@@ -91,19 +90,19 @@ class GmailClient:
         if result != "OK":
             logger.error(f"Error searching for mails on {date}: {data}")
             return []
-        message_uids = list(map(int, data[0].split()))
+        message_uids = data[0].decode().split()
         logger.info(f"Found {len(message_uids)} messages on {date}")
         return message_uids
 
-    def search_uids_by_subject(self, subject: str) -> List[int]:
+    def search_uids_by_subject(self, subject: str) -> List[str]:
         """Return UIDs of messages with a specific subject."""
-        result, data = self.mail_server.uid("search", None, f'(SUBJECT "{subject}")')
-        if result != "OK":
-            logger.error(f"Error searching for subject '{subject}': {data}")
-            return []
-        return list(map(int, data[0].split()))
+        search_string = f'(HEADER Subject "{subject}")'
+        status, data = self.mail_server.uid("search", None, search_string)
+        if status != "OK":
+            raise RuntimeError("Failed to search by subject")
+        return data[0].decode().split()
 
-    def get_uids_by_sbd_imei(self, imei: str | int) -> List[int]:
+    def get_uids_by_sbd_imei(self, imei: str) -> List[str]:
         """
         Return Gmail UIDs of Iridium SBD messages for the specified IMEI number.
         """
@@ -121,9 +120,9 @@ class GmailClient:
         parser.read([str(p) for p in config_files])
 
         return cls(
-            server=parser.get("imap", "server"),
-            port=parser.getint("imap", "port"),
-            account=parser.get("imap", "account"),
-            password=parser.get("imap", "password"),
+            server=parser.get("aws", "server"),
+            port=parser.getint("aws", "port"),
+            account=parser.get("aws", "account"),
+            password=parser.get("aws", "password"),
         )
 
