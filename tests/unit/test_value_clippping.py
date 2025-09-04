db47c2081b2ed55e645ef5b4f1f8b3e51db5497f
Penny How <pho@geus.dk>
2025-09-04 14:26:12 -0100

    core rename

--- tests/unit/test_value_clippping.py
+++ tests/unit/test_value_clippping.py
@@ -5,7 +5,7 @@ import pandas as pd
 import xarray as xr
 
 import pypromice.resources
-from pypromice.algorithms.wind import filter_wind_direction, calculate_directional_wind_speed
+from pypromice.core.wind import filter_wind_direction, calculate_directional_wind_speed
 from pypromice.qc.value_clipping import clip_values
 
 
