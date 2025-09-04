db7ac7b84f116874ec33b38f238fae0ba6c83777
PennyHow <pho@geus.dk>
2025-09-04 15:11:32 -0100

    conflict resolve

--- tests/unit/test_value_clippping.py
+++ tests/unit/test_value_clippping.py
@@ -5,7 +5,7 @@ import pandas as pd
 import xarray as xr
 
 import pypromice.resources
-from pypromice.core.wind import filter_wind_direction, calculate_directional_wind_speed
+from pypromice.core.variables.wind import filter_wind_direction, calculate_directional_wind_speed
 from pypromice.qc.value_clipping import clip_values
 
 
