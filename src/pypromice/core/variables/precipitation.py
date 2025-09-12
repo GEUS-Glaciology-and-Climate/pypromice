825accecac3b527ef3a5c5a4c4ddb98a56f49ceb
PennyHow <pho@geus.dk>
2025-09-12 12:04:54 -0100

    correct and filter functions added

---
+++ src/pypromice/core/variables/precipitation.py
@@ -0,0 +1,101 @@
+__all__ = ["correct", "filter"]
+
+import numpy as np
+import xarray as xr
+
+def filter(precip: xr.DataArray,
+           t: xr.DataArray,
+           p: xr.DataArray,
+           rh: xr.DataArray
+) -> xr.DataArray:
+    """Filter precipitation measurements where air temperature, pressure, or
+    relative humidity measurements are null values. This assumes that
+    air temperature, air pressure, relative humidity and precipitation
+    measurements are measured using the same instrument, e.g. a
+    lufft instrument.
+
+    Parameters
+    ----------
+    precip : xr.DataArray
+        Cumulative precipitation measurements
+    t : xr.DataArray
+        Air temperature measurements
+    p : xr.DataArray
+        Air pressure measurements
+    rh : xr.DataArray
+        Relative humidity measurements
+
+    Returns
+    -------
+    xr.DataArray
+        Filtered precipitation values
+    """
+    mask = (t.isnull() | p.isnull() | rh.isnull()) & (precip == 0)
+    return precip.where(~mask)
+
+def correct(precip: xr.DataArray,
+            wspd: xr.DataArray,
+            t: xr.DataArray
+) -> xr.DataArray:
+    """Correct precipitation with the undercatch correction method used in
+    Yang et al. (1999) and Box et al. (2022), based on Goodison et al. (1998).
+
+    Yang, D., Ishida, S., Goodison, B. E., and Gunther, T.: Bias correction of
+    daily precipitation measurements for Greenland,
+    https://doi.org/10.1029/1998jd200110, 1999.
+
+    Box, J., Wehrle, A., van As, D., Fausto, R., Kjeldsen, K., Dachauer, A.,
+    Ahlstrom, A. P., and Picard, G.: Greenland Ice Sheet rainfall, heat and
+    albedo feedback imapacts from the Mid-August 2021 atmospheric river,
+    Geophys. Res. Lett. 49 (11), e2021GL097356,
+    https://doi.org/10.1029/2021GL097356, 2022.
+
+    Goodison, B. E., Louie, P. Y. T., and Yang, D.: Solid Precipitation
+    Measurement Intercomparison, WMO, 1998
+
+    Parameters
+    ----------
+    precip : xr.DataArray
+        Cumulative precipitation measurements
+    wspd : xr.DataArray
+        Wind speed measurements
+    t : xr.DataArray
+        Air temperature measurements
+
+    Returns
+    -------
+    precip_rate : xr.DataArray
+        Corrected precipitation rate
+    """
+    nan_mask = precip.isnull()
+
+    # Calculate undercatch correction factor
+    corr=100/(100.00-4.37*wspd+0.35*wspd*wspd)
+
+    # Fix all values below 1.02 to 1.02
+    corr = corr.where(corr>1.02, other=1.02)
+
+    # Fill nan values in precip with preceding value
+    precip = precip.ffill(dim='time')
+
+    # Calculate precipitation rate and makes sure it remains of the same size
+    # and taking into account the time step size to ensure mm/hr
+    dt_hours = precip['time'].diff('time') / np.timedelta64(1, 'h')
+    dt_hours = dt_hours.reindex_like(precip)
+    precip_rate = precip.diff('time') / dt_hours
+
+    # Apply correction to rate
+    precip_rate = precip_rate*corr
+
+    # Removing all negative precipitation rates
+    precip_rate = precip_rate.where(precip_rate>0)
+
+    # Filtering cold season precipitation measurements
+    rain_in_cold = (precip_rate>0) & (t<-2)
+    precip_rate = precip_rate.where(~rain_in_cold)
+
+    # Removing timestamps where precipitation rates have been calculated over
+    # interpolated values
+    precip_rate = precip_rate.where(~nan_mask)
+
+    return precip_rate
--- src/pypromice/pipeline/L1toL2.py
+++ src/pypromice/pipeline/L1toL2.py
@@ -13,7 +13,7 @@ from pypromice.core.qc.github_data_issues import flagNAN, adjustTime, adjustData
 from pypromice.core.qc.percentiles.outlier_detector import ThresholdBasedOutlierDetector
 from pypromice.core.qc.persistence import persistence_qc
 from pypromice.core.qc.value_clipping import clip_values
-from pypromice.core.variables import wind
+from pypromice.core.variables import wind, precipitation
 
 __all__ = [
     "toL2",
@@ -135,23 +135,18 @@ def toL2(
     ds, _ = process_sw_radiation(ds)
 
     # Correct precipitation
-    if hasattr(ds, 'correct_precip'):
-        precip_flag = ds.attrs['correct_precip']
+    if hasattr(ds, "correct_precip"):
+        precip_flag = ds.attrs["correct_precip"]
     else:
         precip_flag=True
-    if ~ds['precip_u'].isnull().all() and precip_flag:
-        ds['precip_rate_u'] = process_luft_precipitation(ds['precip_u'],
-                                                                ds['wspd_u'],
-                                                                ds['t_u'],
-                                                                ds['p_u'],
-                                                                ds['rh_u'])
-    if ds.attrs['number_of_booms']==2:
-        if ~ds['precip_l'].isnull().all() and precip_flag:                     # Correct precipitation
-            ds['precip_rate_l']= process_luft_precipitation(ds['precip_l'],
-                                                                   ds['wspd_l'],
-                                                                   ds['t_l'],
-                                                                   ds['p_l'],
-                                                                   ds['rh_l'])
+    if ~ds["precip_u"].isnull().all() and precip_flag:
+        ds["precip_u"] = precipitation.mask(ds["precip_u"], ds["t_u"], ds["p_u"], ds["rh_u"])
+        ds["precip_rate_u"] = precipitation.correct(ds["precip_u"], ds["wspd_u"], ds["t_u"])
+
+    if ds.attrs["number_of_booms"]==2:
+        if ~ds["precip_l"].isnull().all() and precip_flag:
+                ds["precip_l"] = precipitation.mask(ds["precip_l"], ds["t_l"], ds["p_l"], ds["rh_l"])
+                ds["precip_rate_l"] = precipitation.correct(ds["precip_l"], ds["wspd_l"], ds["t_l"])
 
     # Calculate directional wind speed for upper boom
     ds['wdir_u'] = wind.filter_wind_direction(ds['wdir_u'],
@@ -483,75 +478,6 @@ def adjustHumidity(rh, T, T_0, T_100, ews, ei0):
     # Set to Groff & Gratch values when freezing, otherwise just rh
     rh_wrt_ice_or_water = rh.where(~freezing, other = rh*(e_s_wtr / e_s_ice))
     return rh_wrt_ice_or_water
-
-
-def process_luft_precipitation(precip, wspd, t, p, rh):
-    '''Correct precipitation with the undercatch correction method used in
-    Yang et al. (1999) and Box et al. (2022), based on Goodison et al. (1998)
-
-    Yang, D., Ishida, S., Goodison, B. E., and Gunther, T.: Bias correction of
-    daily precipitation measurements for Greenland,
-    https://doi.org/10.1029/1998jd200110, 1999.
-
-    Box, J., Wehrle, A., van As, D., Fausto, R., Kjeldsen, K., Dachauer, A.,
-    Ahlstrom, A. P., and Picard, G.: Greenland Ice Sheet rainfall, heat and
-    albedo feedback imapacts from the Mid-August 2021 atmospheric river,
-    Geophys. Res. Lett. 49 (11), e2021GL097356,
-    https://doi.org/10.1029/2021GL097356, 2022.
-
-    Goodison, B. E., Louie, P. Y. T., and Yang, D.: Solid Precipitation
-    Measurement Intercomparison, WMO, 1998
-
-    Parameters
-    ----------
-    precip : xarray.DataArray
-        Cumulative precipitation measurements
-    wspd : xarray.DataArray
-        Wind speed measurements
-
-    Returns
-    -------
-    precip_cor : xarray.DataArray
-        Cumulative precipitation corrected
-    precip_rate : xarray.DataArray
-        Precipitation rate corrected
-    '''
-    # filter time steps where t, p or rh are not available, meaning that the Lufft is not working
-    mask = (t.isnull() | p.isnull() | rh.isnull()) & (precip == 0)
-    precip = precip.where(~mask)
-
-    nan_mask = precip.isnull()
-
-    # Calculate undercatch correction factor
-    corr=100/(100.00-4.37*wspd+0.35*wspd*wspd)
-
-    # Fix all values below 1.02 to 1.02
-    corr = corr.where(corr>1.02, other=1.02)
-
-    # Fill nan values in precip with preceding value
-    precip = precip.ffill(dim='time')
-
-    # Calculate precipitation rate and makes sure it remains of the same size
-    # and taking into account the time step size to ensure mm/hr
-    dt_hours = precip['time'].diff('time') / np.timedelta64(1, 'h')
-    dt_hours = dt_hours.reindex_like(precip)
-    precip_rate = precip.diff('time') / dt_hours
-
-    # Apply correction to rate
-    precip_rate = precip_rate*corr
-
-    # Removing all negative precipitation rates
-    precip_rate = precip_rate.where(precip_rate>0)
-
-    # filtering cold season precipitation measurements
-    rain_in_cold = (precip_rate>0) & (t<-2)
-    precip_rate = precip_rate.where(~rain_in_cold)
-
-    # removing timestamps where precitipation rates have been calculated over
-    # interpolated values
-    precip_rate = precip_rate.where(~nan_mask)
-
-    return precip_rate
 
 
 def calcDeclination(doy, hour, minute):
