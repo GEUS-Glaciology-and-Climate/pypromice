61c5588545483df95afdae2d485c3a21eeb0e5fe
BaptisteVandecrux <b.vandecrux@gmail.com>
2025-10-04 10:14:24 +0200

    Update station_boom_height.py

--- src/pypromice/core/variables/station_boom_height.py
+++ src/pypromice/core/variables/station_boom_height.py
@@ -25,25 +25,47 @@ def adjust(z_boom: xr.DataArray,
     return z_boom * ((air_temperature + T_0)/T_0)**0.5
 
 
-def adjust_and_include_uncorrected_values(z_boom: xr.DataArray,
-           air_temperature: xr.DataArray
+def include_uncorrected_values(
+    z_boom: xr.DataArray,
+    z_boom_cor: xr.DataArray,
+    air_temperature_1: xr.DataArray,
+    air_temperature_2: xr.DataArray = None,
+    t_rad: xr.DataArray = None,
+    T_0: float = 273.15,
 ) -> xr.DataArray:
-    """Adjust sonic ranger readings for sensitivity to air temperature,
-    and retain uncorrected values where air temperature measurements
-    are not available.
+    """
+    Adjust sonic ranger readings for sensitivity to air temperature and
+    retain uncorrected values where temperature measurements are unavailable.
 
     Parameters
     ----------
     z_boom : xr.DataArray
-        Station boom height from sonic ranger
-    air_temperature : xr.DataArray
-        Air temperature
+        Uncorrected station boom height from sonic ranger
+    z_boom_cor : xr.DataArray
+        Boom height corrected with air_temperature_1
+    air_temperature_1 : xr.DataArray
+        Primary air temperature
+    air_temperature_2 : xr.DataArray, optional
+        Secondary air temperature
+    t_rad : xr.DataArray, optional
+        Radiative temperature
+    T_0 : float, optional
+        Reference temperature in Kelvin (default 273.15)
 
     Returns
     -------
     xr.DataArray
-        Adjusted station boom height
+        Corrected boom height with fallback where needed
     """
-    return xr.where(air_temperature.notnull(),
-                    z_boom * ((air_temperature + T_0)/T_0)**0.5,
-                    z_boom)
+    if air_temperature_2 is None:
+        air_temperature_2 = xr.full_like(z_boom, np.nan)
+    if t_rad is None:
+        t_rad = xr.full_like(z_boom, np.nan)
+    else:
+        t_rad = t_rad.clip(max=0)
+
+    z_boom_cor_w_t_rad = z_boom * ((t_rad + T_0) / T_0) ** 0.5
+    z_boom_cor_w_ta2 = z_boom * ((air_temperature_2 + T_0) / T_0) ** 0.5
+    z_boom_ta2_t_rad = xr.where(air_temperature_2.notnull(), z_boom_cor_w_ta2, z_boom_cor_w_t_rad)
+
+    return xr.where(air_temperature_1.notnull(), z_boom_cor, z_boom_ta2_t_rad)
