96f53aafa6cdcc263a95bd6065443ea08564c77e
Baptiste Vandecrux <35140661+BaptisteVandecrux@users.noreply.github.com>
2025-10-06 14:01:00 +0200

    Bug fix/fixing z boom best (#376)
    In the previous implementation z_boom_best was calculated with adjust_and_include_uncorrected_values which recalculated its own version of corrected z_boom_cor, meaning that all "adjustment" filters applied on the official z_boom_cor outside of the function was not visible in z_boom_best. So this PR does:
    
        - replaced adjust_and_include_uncorrected_values by include_uncorrected_values which just combines uncorrected values with the QCed z_boom_cor
        - include_uncorrected_values tries to use t_l and t_rad if available for the correction
        - set co-dependency of z_* and z_*_cor so that filters on z_*_cor also remove the corresponding data to z_*
        added a custom filter to remove cases where a SR50 is failing and the corresponding value falls back on the other SR50 (https://github.com/GEUS-Glaciology-and-Climate/PROMICE-AWS-data-issues/issues/3)

--- src/pypromice/core/variables/station_boom_height.py
+++ src/pypromice/core/variables/station_boom_height.py
@@ -1,4 +1,4 @@
-__all__ = ["adjust", "adjust_and_include_uncorrected_values"]
+__all__ = ["adjust", "include_uncorrected_values"]
 
 import numpy as np
 import xarray as xr
@@ -25,25 +25,51 @@ def adjust(z_boom: xr.DataArray,
     return z_boom * ((air_temperature + T_0)/T_0)**0.5
 
 
-def adjust_and_include_uncorrected_values(z_boom: xr.DataArray,
-           air_temperature: xr.DataArray
+def include_uncorrected_values(
+    z_boom: xr.DataArray,
+    z_boom_cor: xr.DataArray,
+    air_temperature_other_level: xr.DataArray = None,
+    t_rad: xr.DataArray = None,
+    T_0: float = 273.15,
 ) -> xr.DataArray:
-    """Adjust sonic ranger readings for sensitivity to air temperature,
-    and retain uncorrected values where air temperature measurements
-    are not available.
+    """
+    Adjust sonic ranger readings for sensitivity to air temperature and
+    retain uncorrected values where temperature measurements are unavailable.
 
     Parameters
     ----------
     z_boom : xr.DataArray
-        Station boom height from sonic ranger
-    air_temperature : xr.DataArray
-        Air temperature
+        Uncorrected station boom height from sonic ranger
+    z_boom_cor : xr.DataArray
+        Boom height corrected with air_temperature_1
+    air_temperature_other_level : xr.DataArray, optional
+        Secondary air temperature
+    t_rad : xr.DataArray, optional
+        Radiative temperature
+    T_0 : float, optional
+        Reference temperature in Kelvin (default 273.15)
 
     Returns
     -------
     xr.DataArray
-        Adjusted station boom height
+        Corrected boom height with fallback where needed
     """
-    return xr.where(air_temperature.notnull(),
-                    z_boom * ((air_temperature + T_0)/T_0)**0.5,
-                    z_boom)
+    if air_temperature_other_level is None:
+        air_temperature_other_level = xr.full_like(z_boom, np.nan)
+    if t_rad is None:
+        t_rad = xr.full_like(z_boom, np.nan)
+    else:
+        t_rad = t_rad.clip(max=0)
+    
+    # first gap filling using values corrected with air_temperature_other_level
+    z_boom_cor_w_ta2 = z_boom * ((air_temperature_other_level + T_0) / T_0) ** 0.5
+    z_boom_cor=z_boom_cor.fillna(z_boom_cor_w_ta2)
+
+    # second gap filling using values corrected with t_rad
+    z_boom_cor_w_t_rad = z_boom * ((t_rad + T_0) / T_0) ** 0.5
+    z_boom_cor=z_boom_cor.fillna(z_boom_cor_w_t_rad)
+
+    # third gap filling using uncorrected values
+    z_boom_cor=z_boom_cor.fillna(z_boom)
+
+    return z_boom_cor
