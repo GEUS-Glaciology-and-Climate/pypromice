2e5bbddad1dfe35834f4f5ecf43eacd7d668a209
BaptisteVandecrux <b.vandecrux@gmail.com>
2025-10-06 13:11:13 +0200

    lowered completeness threshold for z_*
    V2 stations transmit z_boom and z_stake every 6 hours

--- src/pypromice/core/resampling.py
+++ src/pypromice/core/resampling.py
@@ -7,12 +7,12 @@ DEFAULT_COMPLETENESS_THRESHOLDS = {
     "albedo": 0.2,
     "p_u": 0.5,
     "p_l": 0.5,
-    "z_boom_u": 0.5,
-    "z_boom_l": 0.5,
-    "z_boom_cor_u": 0.5,
-    "z_boom_cor_l": 0.5,
-    "z_stake": 0.5,
-    "z_stake_cor": 0.5,
+    "z_boom_u": 0.1,
+    "z_boom_l": 0.1,
+    "z_boom_cor_u": 0.1,
+    "z_boom_cor_l": 0.1,
+    "z_stake": 0.1,
+    "z_stake_cor": 0.1,
     "z_surf_combined": 0.1,
 }
 
