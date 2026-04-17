fa79d7016b545ee6f12436765192b44a19f157bb
Baptiste Vandecrux <35140661+BaptisteVandecrux@users.noreply.github.com>
2026-04-17 15:17:22 +0200

    version bump

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2025, GEUS Glaciology and Climate'
 author = 'GEUS Glaciology and Climate'
 
 # The full version, including alpha/beta/rc tags
-release = '1.10.2'
+release = '1.10.3'
 
 
 # -- General configuration ---------------------------------------------------
